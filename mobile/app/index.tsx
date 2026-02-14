7d49422294a1cf9f102e0b3db02d058bc66b9646
sourodip19 <sourodipdey19@gmail.com>
2026-02-14 14:36:16 +0530

    refactor: remove unused components and hooks, integrate Tailwind CSS with NativeWind
    - Deleted components: ExternalLink, HapticTab, HelloWave, ParallaxScrollView, ThemedText, ThemedView, Collapsible, IconSymbol (iOS and fallback), and theme constants.
    - Removed hooks: useColorScheme and useThemeColor.
    - Added global CSS file for Tailwind and configured Metro bundler for NativeWind.
    - Updated package.json to include NativeWind and Tailwind CSS dependencies.
    - Created tailwind.config.js for Tailwind CSS setup.
    - Adjusted tsconfig.json to include nativewind-env.d.ts for TypeScript support.

---
+++ mobile/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View>
+      <Text className="text-red-500 text-4xl">
+        Edit app/index.tsx to edit this screen.hello mfs
+      </Text>
+    </View>
+  );
+}
